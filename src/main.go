5de79558feb896ad91b1bb27076cc2542016f092
devhindo <ahmedhindawi2001@gmail.com>
2023-08-16 12:39:40 +0300

    finished

---
+++ src/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) >= 2 {
+		fmt.Println("Hello", os.Args[1])
+	} else {
+		fmt.Println("Hello World")
+	}
+	println("")
+	
+}
